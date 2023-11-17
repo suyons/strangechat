19894ebbd3aad1c8ed183cb12d31ca2d17c09735
suyons <github.choice044@passmail.com>
2023-11-17 21:15:01 +0900

    MultiServer NullPointerException 오류 수정

--- src/ExtermalSourceCode/MultiClient.java
+++ src/ExtermalSourceCode/MultiClient.java
@@ -1,6 +1,5 @@
 package ExtermalSourceCode;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,40 +8,35 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class MultiClient {
-	
+
 	public static void main(String[] args) {
 		MultiClient multiClient = new MultiClient();
 		multiClient.start();
 	}
-	
+
 	public void start() {
-		Socket socket = null;
+		// Socket socket = null;
 		BufferedReader in = null;
-		try {
-			socket = new Socket("localhost", 8000);
+		try (Socket socket = new Socket("localhost", 10011)) {
 			System.out.println("[서버와 연결되었습니다]");
 
-			String name = "user" + (int)(Math.random()*10);
+			String name = "user" + (int) (Math.random() * 10);
 			Thread sendThread = new SendThread(socket, name);
 			sendThread.start();
 
 			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			while (in != null) {
 				String inputMsg = in.readLine();
-				if(("[" + name + "]님이 나가셨습니다").equals(inputMsg)) break;
+				if (("[" + name + "]님이 나가셨습니다").equals(inputMsg))
+					break;
 				System.out.println("From:" + inputMsg);
 			}
 		} catch (IOException e) {
 			System.out.println("[서버 접속끊김]");
-		} finally {
-			try {
-				socket.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 		System.out.println("[서버 연결종료]");
 	}
+
 }
 
 class SendThread extends Thread {
@@ -50,7 +44,7 @@ class SendThread extends Thread {
 	String name;
 
 	Scanner scanner = new Scanner(System.in);
-	
+
 	public SendThread(Socket socket, String name) {
 		this.socket = socket;
 		this.name = name;
@@ -58,21 +52,21 @@ class SendThread extends Thread {
 
 	@Override
 	public void run() {
- 		try {
+		try {
 			// 최초1회는 client의 name을 서버에 전송
 			PrintStream out = new PrintStream(socket.getOutputStream());
 			out.println(name);
 			out.flush();
-			
+
 			while (true) {
 				String outputMsg = scanner.nextLine();
 				out.println(outputMsg);
 				out.flush();
-				if("quit".equals(outputMsg)) break;
+				if ("quit".equals(outputMsg))
+					break;
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
-		} 
+		}
 	}
 }
-
--- src/ExtermalSourceCode/MultiServer.java
+++ src/ExtermalSourceCode/MultiServer.java
@@ -21,7 +21,7 @@ public class MultiServer {
 		ServerSocket serverSocket = null;
 		Socket socket = null;
 		try {
-			serverSocket = new ServerSocket(8000);
+			serverSocket = new ServerSocket(10011);
 			while (true) {
 				System.out.println("[클라이언트 연결대기중]");
 				socket = serverSocket.accept();
