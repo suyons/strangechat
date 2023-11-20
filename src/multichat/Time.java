d7777dfcee6926a2c1b26c1c9d0a57636a5bd610
WGCAT <seed9878@naver.com>
2023-11-20 17:23:50 +0900

    멀티스레드

---
+++ src/multichat/Time.java
@@ -0,0 +1,12 @@
+package multichat;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+public class Time {
+	public String timeInfo() {
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+		SimpleDateFormat time = new SimpleDateFormat ("aa hh:mm");
+		return (String)time.format(timestamp);
+		}
+	}
