79e7090e7f5efb36a1b61795e523c43eeacb5f0d
parkje72 <qkrwldms991621@gmail.com>
2023-11-21 10:53:06 +0900

    파일 옮김

---
+++ src/common/Chat.java
@@ -0,0 +1,41 @@
+package common;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Chat {
+    // Date() 생성자: 인스턴스가 생성된 시각으로 초기화
+    private Date now = new Date();
+    // chatMap의 Key: UNIX timestamp
+    public final long timestamp;
+    // chatMap의 Value: 대화내용
+    public String content = "";
+	public String onlyContent; //입력된 메시지만
+
+    // 생성자 → 현재 시각의 UNIX Timestamp를 입력
+    /* UNIX 시간: 1970-01-01 00:00 (UTC)부터 지금까지 몇 초 지났니? */
+    public Chat() {
+        timestamp = now.getTime();
+    }
+
+    // 연-월-일 시:분:초 형식으로 반환
+    public static String dateTime(long stamp) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String dateTime = sdf.format(new Date(stamp));
+        return dateTime;
+    }
+
+    // (시:분) 형식으로 반환
+    public static String hourMinute(long stamp) {
+        SimpleDateFormat sdf = new SimpleDateFormat("(HH:mm) ");
+        String hourMins = sdf.format(new Date(stamp));
+        return hourMins;
+    }
+
+    // toString 메서드를 재정의
+    // print(Chat.content) 대신 print(Chat)으로 대화내용 출력
+    @Override
+    public String toString() {
+       return this.content;
+    }
+}
---
+++ src/server/ServerApp.java
@@ -0,0 +1,75 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+import common.*;
+
+public class ServerApp {
+    public static void main(String[] args) {
+        // IP주소와 포트를 지정하여 ServerSocket 객체 생성
+        try (ServerSocket serverSocket = new ServerSocket(Constants.SERVER_PORT)) {
+            System.out.println(ChatServer.waiting());            
+
+            while (true) {
+                // serverSocket.accept(): 연결된 Socket 객체를 반환
+                Socket clientSocket = serverSocket.accept();
+                // V HashMap.get(K): HashMap에서 K에 해당하는 V 페어가 없다면 null을 반환
+                
+                if (ChatServer.getUserName(clientSocket.getInetAddress()) == null)
+                    // 기존 접속 기록이 없다면 K-V 페어 레코드를 HashMap에 추가
+                    ChatServer.addUser(new User(clientSocket.getInetAddress()));
+                // "새 클라이언트 연결: IP주소" 출력
+                System.out.println(ChatServer.newClient(clientSocket.getInetAddress()));
+
+                /*
+                 * ■ OutputStream on Network
+                 * (1) Server Side:
+                 * ● The server uses the input stream (getInputStream())
+                 * of the client's socket to read data sent by the client.
+                 * ● The server uses the output stream (getOutputStream())
+                 * of the client's socket to send data to the client.
+                 * (2) Client Side:
+                 * ● Conversely, the client uses the input stream
+                 * of the server's socket to read data sent by the server.
+                 * ● The client uses the output stream of its own socket
+                 * to send data to the server.
+                 */
+
+                // 서버 → 클라이언트 전송: 서버에서 클라이언트 소켓의 출력 스트림 사용
+                PrintWriter writer = new PrintWriter(
+                        new BufferedWriter(
+                                new OutputStreamWriter(
+                                        clientSocket.getOutputStream(), StandardCharsets.UTF_8)),
+                        true);
+
+                // 클라이언트 접속 시 "OOOO님 반갑습니다!" 출력
+                writer.println(ChatServer.greeting(clientSocket));
+
+                // 클라이언트 → 서버 전송: 서버에서 클라이언트 소켓의 입력 스트림 사용
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(
+                                clientSocket.getInputStream(), StandardCharsets.UTF_8));
+
+                // 클라이언트로부터 받은 메시지를 서버에서 출력하고, 클라이언트에게도 송출
+                String content;
+                while ((content = reader.readLine()) != null) {
+                    Chat chat = ChatServer.clientsChat(clientSocket.getInetAddress(), content);
+                    System.out.println(chat);
+                    writer.println(chat);
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+        }
+    }
+}
